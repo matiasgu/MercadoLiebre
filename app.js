d9ce0b709bef603ba727feeec1093d601bb289d8
matiasgu <matias.gutierrez4989@gmail.com>
2024-01-22 20:25:11 -0300

    cambio

--- app.js
+++ app.js
@@ -3,7 +3,8 @@ const app = express();
 const path = require('path');
 
 app.use(express.static('public'));
-app.listen(3000, () => console.log('Server running at http://localhost:3000'));
+const PORT = process.env.PORT || 3000;
+app.listen( PORT, () => console.log(`Server up on PORT:  http://localhost:${PORT}`) )
 
 app.get('/', (req, res) =>{
     res.sendFile(path.join(__dirname, '/views/home.html'));
